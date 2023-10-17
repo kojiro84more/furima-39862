2eb587ba4bfa90e62da442af4ddd8893e8140c6e
kojiro84more <minimax.hashimoto@gmail.com>
2023-10-18 06:56:31 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  
   belongs_to :category
   belongs_to :prefecture
   belongs_to :item_status
@@ -22,7 +23,7 @@ class Item < ApplicationRecord
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
-  with_options numericality: { other_than: 0, message: "can't be blank" } do
+  with_options numericality: { other_than: 0, } do
     validates :category_id
     validates :prefecture_id
     validates :item_status_id
