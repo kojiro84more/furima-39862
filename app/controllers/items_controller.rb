b3e9a38c71ad0b1da21c77d673778e5d96f8bf92
kojiro84more <minimax.hashimoto@gmail.com>
2023-10-18 19:41:38 +0900

    商品一覧表示機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,8 @@ class ItemsController < ApplicationController
   
   
   def index
+    @items = Item.all
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -26,8 +28,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id,
-                                 :shipping_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id,:shipping_date_id, :price).merge(user_id: current_user.id)
   end
 
 end
